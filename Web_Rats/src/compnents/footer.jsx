513fdbab262038442ef1055ca0ac6eab09b66476
ABIJITH-K-A <abijithkrishnakumar07@gmail.com>
2026-03-20 14:32:48 +0530

    basement created

---
+++ Web_Rats/src/compnents/footer.jsx
@@ -0,0 +1,31 @@
+function footer() {
+    return (
+        <footer>
+            <div>
+                <div>
+                    <div>
+                        <h5>WEB RATS</h5>
+                        <p>We Create</p>
+                    </div>
+                    <div>
+                        Contact Info
+                        Example@gmail.com
+                        +91 8300920680
+                        @tn_web_rats
+                    </div>
+                    <div>
+                        <h6>Quick Links</h6>
+                        <ul>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">Book Service</a></li>
+                            <li><a href="">About</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <p>© 2026 TN Web Rats. All rights reserved. | Privacy Policy | Terms</p>
+            </div>
+        </footer>
+    )
+}
+
+export default footer
