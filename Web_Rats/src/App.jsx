1b50c5d741c21c415c9ab0b817854a7ef3d532f7
ABIJITH-K-A <abijithkrishnakumar07@gmail.com>
2026-03-20 14:32:48 +0530

    basement created

---
+++ Web_Rats/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './compnents/navbar'
+import Hero from './compnents/hero'
+import Footer from './compnents/footer'
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <Footer />
+    </>
+  )
+}
+
+export default App
